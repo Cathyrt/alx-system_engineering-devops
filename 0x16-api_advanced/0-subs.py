bf28840b0d66039afeed4e4f2dab957603c7b323
Cathyrt <cathrinert20@icloud.com>
2023-05-09 05:01:01 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Returns the total number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    url = 'http://reddit.com/r/{}/about.json'
+    header = {'User-agent': 'Mozilla/5.0'}
+    res = requests.get(url.format(subreddit), headers=header)
+    if res.status_code != 200:
         return 0
+    return res.json().get('data', {}).get('subscribers', 0)
