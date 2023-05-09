9c72b6bc5b1576a5e1976b45a115a310c331e324
Cathyrt <cathrinert20@icloud.com>
2023-05-09 10:25:30 -0700

    Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,17 @@ def top_ten(subreddit):
     """Retrieves the title of the top ten posts from a given subreddit."""
     url = "http://reddit.com/r/{}/hot.json".format(subreddit)
     header = {'User-agent': 'Mozilla/5.0'}
-    res = requests.get(url, headers=header, allow_redirects=False)
-    inf = res.json().get('data', {}).get('children', {})
+    limits = {'limit': '10'}
+
+    res = requests.get(url, headers=header,
+                       params=limits, allow_redirects=False)
     if res.status_code != 200:
-        return print('None')
-    for x in inf[0:10]:
-        print(x.get('data', {}).get('title'))
+        print('None')
+    else:
+        res_json = res.json()
+
+        if res_json.get('data') and res_json.get('data').get('children'):
+            hot_posts = res_json.get('data').get('children')
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
