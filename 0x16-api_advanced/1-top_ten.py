960421ca925c94e2974775fa948847982a7477f5
Cathyrt <cathrinert20@icloud.com>
2023-05-09 09:13:32 -0700

    Update top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,17 +9,10 @@ import requests
 def top_ten(subreddit):
     """Retrieves the title of the top ten posts from a given subreddit."""
     url = "http://reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
-    res = requests.get(url, headers=header,
-                       params=params, allow_redirects=False)
+    header = {'User-agent': 'Mozilla/5.0'}
+    res = requests.get(url, headers=header, allow_redirects=False)
+    inf = res.json().get('data', {}).get('children', {})
     if res.status_code != 200:
-        print('None')
-        return
-    data = res.json()
-    hot_posts = data['data']['children']
-    if len(hot_posts) == 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+        return print('None')
+    for x in inf[0:10]:
+        print(x.get('data', {}).get('title'))
