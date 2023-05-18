0aad08e9e87af70d2ed9fb6e2374746738f37275
Cathyrt <cathrinert20@icloud.com>
2023-05-17 23:56:13 -0700

    Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fix the number of max open files per process
 
-exec { 'fix--for-nginx':
+exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
 
-exec { 'restart nginx':
+exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
